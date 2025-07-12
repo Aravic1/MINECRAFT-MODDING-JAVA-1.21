1de1b6468e7d900295e5fc7c4d0c32db6133bcb1
Hayden-Mann <hem21@students.uwf.edu>
2025-07-12 08:19:38 -0500

    added 2 new blocks

--- src/main/java/net/aravic/tutorialmod/TutorialMod.java
+++ src/main/java/net/aravic/tutorialmod/TutorialMod.java
@@ -1,6 +1,7 @@
 package net.aravic.tutorialmod;
 
 import com.mojang.logging.LogUtils;
+import net.aravic.tutorialmod.block.ModBlocks;
 import net.aravic.tutorialmod.item.ModItems;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraftforge.api.distmarker.Dist;
@@ -32,6 +33,7 @@ public class TutorialMod {
         MinecraftForge.EVENT_BUS.register(this);
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
 
         // Register the item to a creative tab
@@ -49,6 +51,11 @@ public class TutorialMod {
         if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
             event.accept(ModItems.ALEXANDRITE);
             event.accept(ModItems.RAW_ALEXANDRITE);
+        }
+
+        if(event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS) {
+            event.accept(ModBlocks.ALEXANDRITE_BLOCK);
+            event.accept(ModBlocks.RAW_ALEXANDRITE_BLOCK);
         }
     }
 
